2d25c9bf9edbed2a67abf206454722df02c253bb
Golla Nischala <nischalagolla24@gmail.com>
2022-01-12 00:36:36 +0530

    coupon

---
+++ Homepage/images/Dynamic/dynamic.js
@@ -0,0 +1,21 @@
+let url="http://localhost:4201/Brand";
+
+function getBrand(){
+
+}
+
+function test(){
+    document.getElementById('coupon').style.visibility="visible";
+}
+function clos(){
+    alert("hii");
+    document.getElementById('coupon').style.visibility="hidden";
+}
+
+// function closeDiv(){
+//     document.getElementById('coupon').style.visibility="hidden"
+// }
+
+// function test(){
+//     document.getElementById('coupon').style.visibility="visible"
+// }
--- Homepage/lenskart.js
+++ Homepage/lenskart.js
@@ -1,16 +1,25 @@
-let url="http://localhost:4201/Brand";
+// let url="http://localhost:4201/Brand";
 
-function getBrands(){
-    fetch(url)
-    .then((res) => res.json()); console.log('res');
-    // .then((data) =>{ 
-    //     for(i=0;i<data.length;i++){
-    //         let element =document.createElement('option')
-    //         let text = document.createTextNode(data[i].Brand_name)
-    //         element.appendChild(text)
-    //         element.value = data[i]._id
-    //         document.getElementById('search').appendChild(element)
-    //     }
+// function getBrands(){
+//     fetch(url)
+//     .then((res) => res.json())
+//     .then((data) =>{ 
+//         for(i=0;i<data.length;i++){
+//             let element =document.createElement('option')
+//             let text = document.createTextNode(data[i].Brand_name)
+//             element.appendChild(text)
+//             element.value = data[i]._id
+//             document.getElementById('search').appendChild(element)
+//         }
      
-    // })
-}
+//     })
+// }
+
+
+function closeDiv(){
+        document.getElementById('coupon').style.visibility="hidden"
+    }
+    
+    function test(){
+        document.getElementById('coupon').style.visibility="visible"
+    }
