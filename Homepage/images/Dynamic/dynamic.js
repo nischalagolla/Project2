72b70b0218decd768faf560d29339d1f8a1b8803
Golla Nischala <nischalagolla24@gmail.com>
2022-01-13 20:26:49 +0530

    dynamicdropdown

--- Homepage/images/Dynamic/dynamic.js
+++ Homepage/images/Dynamic/dynamic.js
@@ -1,8 +1,21 @@
 let url="http://localhost:4201/Brand";
 
 function getBrand(){
-
+fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+for(i=0;i<data.length;i++){
+    let element =document.createElement('option')
+    let text = document.createTextNode(data[i].name)
+    element.appendChild(text)
+    element.value =data[i]._id
+    document.getElementById('Brand').appendChild(element)
 }
+    })
+}
+
+
+
 
 function test(){
     document.getElementById('coupon').style.visibility="visible";
--- Homepage/lenskart.js
+++ Homepage/lenskart.js
@@ -1,20 +1,24 @@
-// let url="http://localhost:4201/Brand";
+let url="http://localhost:4201/Brand";
 
-// function getBrands(){
-//     fetch(url)
-//     .then((res) => res.json())
-//     .then((data) =>{ 
-//         for(i=0;i<data.length;i++){
-//             let element =document.createElement('option')
-//             let text = document.createTextNode(data[i].Brand_name)
-//             element.appendChild(text)
-//             element.value = data[i]._id
-//             document.getElementById('search').appendChild(element)
-//         }
-     
-//     })
-// }
+function getBrand(){
+fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+for(i=0;i<data.length;i++){
+    let element =document.createElement('option')
+    let text = document.createTextNode(data[i].name)
+    element.appendChild(text)
+    element.value =data[i]._id
+    document.getElementById('Brand').appendChild(element)
+}
+    })
+}
 
+function changeMode(){
+    var mybody = document.body;
+    // if body have class mydark than remove else add
+    mybody.classList.toggle("mydark")
+}
 
 function closeDiv(){
         document.getElementById('coupon').style.visibility="hidden"
